409b7dccefcc0520e47163652d0aa79432a71a1a
SrivalliChaturvedula <chaturvedulavalli@gmail.com>
2024-08-26 16:40:09 +0530

    Changes in Task10 are done

--- src/Tasks/Task010.py
+++ src/Tasks/Task010.py
@@ -8,7 +8,33 @@ n = 5
 """
 import math
 
-for i in range(1, 6):
-    factorial = math.factorial(i)
-    print(f"The factorial of the number {i} is: {factorial}")
+# import math
+#
+# for i in range(1, 6):
+#     factorial = math.factorial(i)
+#     print(f"The factorial of the number {i} is: {factorial}")
 
+# n = int(input("Enter number: "))
+# if n < 0:
+#     print("Factorial is not defined for negative numbers")
+# elif n == 0 == 1:
+#     print("Factorial is: 1")
+# else:
+#     print(math.factorial(n))
+
+n = int(input("Enter number: "))
+fact = 1
+if n == 0 == 1:
+    fact = 1
+    print("Factorial is: ", 1)
+
+else:
+    for i in range(1, n+1, 1):
+        fact = fact * i
+
+    # i = 1
+    # while(i <= n):
+    #     fact = fact * i
+    #     i = i + 1
+
+print(f"Factorial of number is: {fact}")
